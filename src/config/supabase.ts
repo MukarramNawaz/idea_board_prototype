cbf8622e55c901a98dc791feba1b386c46910737
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2025-02-09 17:33:29 +0500

    Add files via upload

---
+++ src/config/supabase.ts
@@ -0,0 +1,4 @@
+export const SUPABASE_CONFIG = {
+  url: 'https://htzqebigzmskerxwceaa.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0enFlYmlnem1za2VyeHdjZWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2NDQ5OTgsImV4cCI6MjA0ODIyMDk5OH0.qpHx0GDd2wHgAjByYSN_EC3XjdLfQ3AGCctqVmfy9IQ'
+};
